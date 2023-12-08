1ea421c63f9e90bc39c1b5f50301f74f23363b97
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2023-12-09 00:13:42 +0300

    Домашка по 2 уроку

--- 2/webpack.config.js
+++ 2/webpack.config.js
@@ -1,8 +1,23 @@
-const path = require('node:path');
+const path = require("node:path");
 
 module.exports = {
-	devtool: false,
-	mode: 'development',
-
-	// ...ваш конфиг
-}
+  devtool: false,
+  mode: "development",
+  entry: {
+    app: {
+      import: path.resolve(__dirname, "src", "app.js"),
+      dependOn: "vendor",
+    },
+    home: {
+      import: path.resolve(__dirname, "src", "pages", "home.js"),
+      dependOn: "vendor",
+    },
+    login: path.resolve(__dirname, "src", "pages", "login.js"),
+    vendor: ["react", "react-dom"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+};
